b7b1cda4cdc123b5edc43bdd1012fb911177a85a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2025-05-08 13:45:18 +0900

    vault backup: 2025-05-08 13:45:18

--- templaters/scripts/checkFrontMatter.js
+++ templaters/scripts/checkFrontMatter.js
@@ -2,22 +2,6 @@
 module.exports = async function(tp, conditions) {
     const frontmatter = tp.frontmatter; // YAML frontmatter 데이터
 
-    
-    // 모든 조건이 충족되는지 확인
-    if (!areAllConditionsMet(frontmatter, conditions)) {
-        return false;
-    }
-    
-    return true;
-};
-
-/**
- * 프론트매터가 모든 조건을 충족하는지 확인하는 함수
- * @param {Object} frontmatter - 검증할 프론트매터 데이터
- * @param {Object} conditions - 충족해야 할 조건 객체
- * @returns {boolean} 모든 조건이 충족되는지 여부
- */
-function areAllConditionsMet(frontmatter, conditions) {
     for (const [key, expectedValue] of Object.entries(conditions)) {
         const actualValue = frontmatter[key];
         
@@ -25,8 +9,10 @@ function areAllConditionsMet(frontmatter, conditions) {
             return false;
         }
     }
+    
     return true;
-}
+};
+
 
 /**
  * 개별 조건이 충족되는지 확인하는 함수
